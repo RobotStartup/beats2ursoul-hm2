bdf6111fffd627333a80d4943200e0c89ed00240
RobotStartup <chriseslami@selfassemblingsystems.com>
2025-03-30 18:53:08 -0400

    adding original css and front page

--- front-page.php
+++ front-page.php
@@ -1,5 +1,33 @@
-<?php get_header() ?>
+<?php get_header(); ?>
 
-<h1>My home page</h1>
+<div class="main-content">
+    <h1>House Music, Soulful Vibes, Timeless Beats.</h1>
+</div>
 
-<?php get_footer() ?>
+<div class="posts-container">
+    <?php
+    $args = array(
+        'post_type' => 'post',
+        'posts_per_page' => -1,
+    );
+
+    $query = new WP_Query($args);
+
+    if ($query->have_posts()) :
+        while ($query->have_posts()) : $query->the_post();
+    ?>
+            <div class="post">
+                <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                <?php the_excerpt(); ?>
+                <a href="<?php the_permalink(); ?>" class="read-more">Read More</a>
+            </div>
+    <?php
+        endwhile;
+        wp_reset_postdata();
+    else :
+        echo '<p>No posts found.</p>';
+    endif;
+    ?>
+</div>
+
+<?php get_footer(); ?>
