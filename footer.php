b69d65dd50417b06827b79356bc4daa8307d90c8
RobotStartup <chriseslami@selfassemblingsystems.com>
2025-03-30 19:16:07 -0400

    update

--- footer.php
+++ footer.php
@@ -1,5 +1,28 @@
-<p>Footer here</p>
-<?php wp_footer() ?>
+<footer class="site-footer">
+
+    <nav class="site-nav">
+        <?php
+
+        $args = array(
+            'theme_location' => 'footer_menu'
+        );
+
+        ?>
+
+        <?php wp_nav_menu($args); ?>
+    </nav>
+
+    <!-- This is the newsletter plugin to add globally -->
+    <div class="newsletter-section">
+        <?php echo do_shortcode('[newsletter]'); ?>
+    </div>
+
+    <p><?php bloginfo('name'); ?> - &copy; <?php echo date('Y'); ?></p>
+</footer>
+
+</div><!-- container -->
+
+<?php wp_footer(); ?>
 
 </body>
 
