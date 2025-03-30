848dbb69150a613ca1fddaa3d839549edf21a08c
RobotStartup <chriseslami@selfassemblingsystems.com>
2025-03-30 16:42:59 -0400

    update

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<h1>Hellow world</h1>
+<?php get_header(); ?>
+
+<?php
+
+while (have_posts()) {
+    the_post(); ?>
+    <div>
+        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    </div>
+<?php }
+
+?>
+
+<?php get_footer(); ?>
